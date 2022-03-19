c5341551eed7b80c24d65b1a711cd354374640df
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-03-19 11:17:48 -0400

    Move navigation to component

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import Navigation from '../components/Navigation'
+import Header from '../components/Header'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -16,36 +16,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <header>
-          <Navbar className="px-4 py-3" collapseOnSelect expand="lg">
-            <Navbar.Brand href="#home">
-              Good News, Everyone
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-              <Nav>
-                <Nav.Link>Home</Nav.Link>
-                <Nav.Link>About</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-      </header>
+      <Header />
 
       <main className={styles.main}>
         
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <Navigation />
       </footer>
     </div>
   )
