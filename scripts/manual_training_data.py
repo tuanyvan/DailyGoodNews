58a474edcfba3fa4fa506b6da8a352855726b4c2
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-03-19 09:27:15 -0400

    Initialize project

---
+++ scripts/manual_training_data.py
@@ -0,0 +1,13 @@
+import requests
+import csv
+import dotenv
+
+## Get request from NewsAPI of many articles
+
+## Get only the headlines from each article
+
+## Iterate over the list of every article
+
+## Determine p=positive, n=negative, neutral for all other user input
+
+## Append results to .csv
